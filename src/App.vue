0148741336175602477ffe36d498e423949b09cc
miao.ayetony <miao.ayetony@gmail.com>
2022-02-21 15:32:22 +0800

    new project of visualization

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "@/components/home/header";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*css reset*/
+div, span, applet, object, iframe, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+body{
+  background: linear-gradient(to right,rgba(10, 11, 10, 0.38),rgba(24, 45, 13, 0)),url('../public/1636.jpeg') no-repeat;
+  background-size: 100%;
+  transition: background-image 0.5s linear;
 }
 </style>
---
+++ src/components/home/header.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div  class="header-left">
+      <h1>智慧园林管理中心</h1>
+      <h3>厦门市园林植物园</h3>
+    </div>
+    <div class="header-nav">
+      <ul>
+        <li>信息总揽</li>
+        <li>机房监测</li>
+        <li>数字交通</li>
+        <li>游客分析</li>
+        <li>园区经营</li>
+        <li>智慧厕所</li>
+        <li>立体安防</li>
+      </ul>
+    </div>
+    <div class="header-right">
+      <p>12:00:09<span>|</span>厦门<span>|</span><img style="width:30px;height: 30px" src="../../../public/sunshine.png">23°C<i>晴</i></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header"
+}
+</script>
+<style scoped>
+
+.header-left {
+  left: 0;
+  top: 0;
+  right: 0;
+  width: 20%;
+  height: 60px;
+  text-align: left;
+  color: #f1eeee;
+  -webkit-font-smoothing: antialiased;
+  position: absolute;
+  padding: 20px;
+}
+.header-left{
+  line-height: 10px;
+}
+h3{
+  letter-spacing: 5px;
+  text-indent: 6px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+}
+.header-nav{
+  position: absolute;
+  margin-left: 20%;
+  width: 60%;
+  top: 0;
+  /*center and horizontal 多个元素水平居中*/
+  display: -webkit-box;
+  -webkit-box-orient: horizontal;
+  -webkit-box-pack: center;
+  -webkit-box-align: center;
+  display: -moz-box;
+  -moz-box-orient: horizontal;
+  -moz-box-pack: center;
+  -moz-box-align: center;
+  padding: 10px;
+}
+li{
+  float: left;
+  color: #f1eeee;
+  list-style-type: none;
+  font-size: 15px;
+  margin: 20px;
+}
+li:hover{
+  color: #2cec2c;
+}
+.header-right{
+  -webkit-font-smoothing: antialiased;
+  position: absolute;
+  font-size: 22px;
+  top: 0;
+  left: 80%;
+  width: 20%;
+  color: #f1eeee;
+  padding: 20px;
+}
+p{
+  float: right;
+  margin-right: 45px;
+}
+span{
+  color: #181717;
+  margin: 10px;
+}
+i{
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 15px;
+  font-weight: bolder;
+  margin-left: 5px;
+  color: white;
+}
+
+</style>
